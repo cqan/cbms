61088e4685d7c2a02c64525b695dadc1dacdcb89
wuhui <slaton@qq.com>
2014-11-26 22:22:47 +0800

    删除多余的代码

--- cms.sql
+++ cms.sql
@@ -10,60 +10,39 @@ Target Server Type    : MYSQL
 Target Server Version : 50173
 File Encoding         : 65001
 
-Date: 2014-11-25 23:20:22
+Date: 2014-11-26 22:22:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for sys_menu
+-- Table structure for permission
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_menu`;
-CREATE TABLE `sys_menu` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `icon` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `pid` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_4mryse4we16s40fdeff06fkrl` (`pid`),
-  CONSTRAINT `FK_4mryse4we16s40fdeff06fkrl` FOREIGN KEY (`pid`) REFERENCES `sys_menu` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of sys_menu
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_permission
--- ----------------------------
-DROP TABLE IF EXISTS `sys_permission`;
-CREATE TABLE `sys_permission` (
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `permission_code` varchar(255) DEFAULT NULL,
   `status` int(11) NOT NULL,
   `pid` bigint(20) DEFAULT NULL,
-  `menu_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_cj5qpen847f3vkcgy7lsmqyug` (`pid`),
-  KEY `FK_t0s0h8w450gp9cumm98x4fymv` (`menu_id`),
-  CONSTRAINT `FK_t0s0h8w450gp9cumm98x4fymv` FOREIGN KEY (`menu_id`) REFERENCES `sys_menu` (`id`),
-  CONSTRAINT `FK_cj5qpen847f3vkcgy7lsmqyug` FOREIGN KEY (`pid`) REFERENCES `sys_permission` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  KEY `FK_19cm2v2q9irnibyextbdgkxmi` (`pid`),
+  CONSTRAINT `FK_19cm2v2q9irnibyextbdgkxmi` FOREIGN KEY (`pid`) REFERENCES `permission` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_permission
+-- Records of permission
 -- ----------------------------
-INSERT INTO `sys_permission` VALUES ('1', '系统管理', 'system', '1', null, null);
-INSERT INTO `sys_permission` VALUES ('2', '用户管理', 'system.user', '1', '1', null);
+INSERT INTO `permission` VALUES ('1', '系统管理', 'system', '1', null);
+INSERT INTO `permission` VALUES ('2', '用户管理', 'system.user', '1', '1');
+INSERT INTO `permission` VALUES ('3', '角色管理', 'system.role', '1', '1');
+INSERT INTO `permission` VALUES ('4', '权限管理', 'system.permission', '1', '1');
 
 -- ----------------------------
--- Table structure for sys_role
+-- Table structure for role
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_role`;
-CREATE TABLE `sys_role` (
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `create_time` datetime DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -72,34 +51,36 @@ CREATE TABLE `sys_role` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_role
+-- Records of role
 -- ----------------------------
-INSERT INTO `sys_role` VALUES ('1', '2014-11-25 23:17:25', '系统管理员', 'admin');
+INSERT INTO `role` VALUES ('1', '2014-11-26 22:16:41', '系统管理', 'admin');
 
 -- ----------------------------
--- Table structure for sys_role_permission
+-- Table structure for role_permission
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_role_permission`;
-CREATE TABLE `sys_role_permission` (
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission` (
   `rid` bigint(20) NOT NULL,
   `pid` bigint(20) NOT NULL,
-  KEY `FK_saergpk0ow0ybq9d6kd2p3kjt` (`pid`),
-  KEY `FK_fxgijgpmm679dpourn5g65seg` (`rid`),
-  CONSTRAINT `FK_fxgijgpmm679dpourn5g65seg` FOREIGN KEY (`rid`) REFERENCES `sys_role` (`id`),
-  CONSTRAINT `FK_saergpk0ow0ybq9d6kd2p3kjt` FOREIGN KEY (`pid`) REFERENCES `sys_permission` (`id`)
+  KEY `FK_4ynr9t0w2nob78a192y84k7lr` (`pid`),
+  KEY `FK_dp6gwvoc3ytvbckpfp8ar7jkd` (`rid`),
+  CONSTRAINT `FK_dp6gwvoc3ytvbckpfp8ar7jkd` FOREIGN KEY (`rid`) REFERENCES `role` (`id`),
+  CONSTRAINT `FK_4ynr9t0w2nob78a192y84k7lr` FOREIGN KEY (`pid`) REFERENCES `permission` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_role_permission
+-- Records of role_permission
 -- ----------------------------
-INSERT INTO `sys_role_permission` VALUES ('1', '1');
-INSERT INTO `sys_role_permission` VALUES ('1', '2');
+INSERT INTO `role_permission` VALUES ('1', '1');
+INSERT INTO `role_permission` VALUES ('1', '2');
+INSERT INTO `role_permission` VALUES ('1', '3');
+INSERT INTO `role_permission` VALUES ('1', '4');
 
 -- ----------------------------
--- Table structure for sys_user
+-- Table structure for user
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `avatar` varchar(255) DEFAULT NULL,
   `birthday` datetime DEFAULT NULL,
@@ -116,24 +97,23 @@ CREATE TABLE `sys_user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_user
+-- Records of user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', null, null, '2014-11-25 23:12:39', null, null, null, '3ef7164d1f6167cb9f2658c07d3c2f0a', '系统管理员', '1', '2014-11-25 23:12:48', 'admin');
+INSERT INTO `user` VALUES ('1', null, '2014-11-26 22:19:24', '2014-11-26 22:19:27', 'slaton@qq.com', '192.168.7.1', '管理员', '3ef7164d1f6167cb9f2658c07d3c2f0a', '吴辉', '1', '2014-11-26 22:20:27', 'admin');
 
 -- ----------------------------
--- Table structure for sys_user_role
+-- Table structure for user_role
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_user_role`;
-CREATE TABLE `sys_user_role` (
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
   `uid` bigint(20) NOT NULL,
   `rid` bigint(20) NOT NULL,
-  KEY `FK_fkfjcdt5mo9svirky5u0kvo9l` (`rid`),
-  KEY `FK_p2ufwk397udfuakvsnok5wqmf` (`uid`),
-  CONSTRAINT `FK_p2ufwk397udfuakvsnok5wqmf` FOREIGN KEY (`uid`) REFERENCES `sys_user` (`id`),
-  CONSTRAINT `FK_fkfjcdt5mo9svirky5u0kvo9l` FOREIGN KEY (`rid`) REFERENCES `sys_role` (`id`)
+  KEY `FK_q7oskmwmmyn5usc6cqsi2g28g` (`rid`),
+  KEY `FK_r5y4sx1dta0whavqah04r5uu8` (`uid`),
+  CONSTRAINT `FK_r5y4sx1dta0whavqah04r5uu8` FOREIGN KEY (`uid`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_q7oskmwmmyn5usc6cqsi2g28g` FOREIGN KEY (`rid`) REFERENCES `role` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of sys_user_role
+-- Records of user_role
 -- ----------------------------
-INSERT INTO `sys_user_role` VALUES ('1', '1');
