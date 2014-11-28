129c113ebbccce7826b8f2601fa3ffe06b8583b6
slatonwu <slaton@qq.com>
2014-11-29 01:38:29 +0800

    增加角色，权限查询

--- cbms-web/src/main/java/com/cqan/controller/UserController.java
+++ cbms-web/src/main/java/com/cqan/controller/UserController.java
@@ -2,18 +2,9 @@ package com.cqan.controller;
 
 import com.cqan.service.UserService;
 import com.cqan.system.User;
-import com.cqan.util.Servlets;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 /**
  * Created by Administrator on 2014/10/19.
