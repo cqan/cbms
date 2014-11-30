3d8a2b755ea01a5cf3b04b37ff9f38f09e68cf44
slatonwu <slaton@qq.com>
2014-11-30 13:56:32 +0800

    sdf

---
+++ cbms-entity/src/main/java/com/cqan/system/Card.java
@@ -0,0 +1,9 @@
+package com.cqan.system;
+
+import com.cqan.IdLongEntity;
+
+public class Card extends IdLongEntity{
+	
+	//private 
+
+}
---
+++ cbms-entity/src/main/java/com/cqan/system/CardStrategy.java
@@ -0,0 +1,72 @@
+package com.cqan.system;
+
+import java.util.Date;
+
+import com.cqan.IdLongEntity;
+
+public class CardStrategy extends IdLongEntity {
+
+	
+	private String name;
+	
+	private String area;
+	
+	private float price;
+	
+	private int cardNum;
+	
+	private Date endTime;
+	
+	private String decription;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getCardNum() {
+		return cardNum;
+	}
+
+	public void setCardNum(int cardNum) {
+		this.cardNum = cardNum;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+	
+	
+	
+}
